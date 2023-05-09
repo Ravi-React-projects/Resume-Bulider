7ccb80139877ab6c48d6af8ec7b0bafcf04bc578
Ravi-React-projects <132327092+Ravi-React-projects@users.noreply.github.com>
2023-05-09 04:00:31 +0000

    edited

---
+++ src/component/form_component/PersonalDetail.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function PersonalDetail() {
+  return (
+    <div>
+        <div className=''>
+          <input type='text' placeholder='First Name'/>
+          <input type='text' placeholder='Middle Name'/>
+          <input type='text' placeholder='Last Name'/>
+        </div>
+    </div>
+  )
+}
+
+export default PersonalDetail
